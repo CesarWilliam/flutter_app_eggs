52f4d002ef5a4772358a6a3acc5f5eaf765925c6
william.rocha <william.rocha@bliteti.com.br>
2020-05-01 12:43:20 -0300

    finalizacao app

---
+++ lib/widgets/action-buttons.widget.dart
@@ -0,0 +1,41 @@
+import 'package:eggs_app/blocs/app.bloc.dart';
+import 'package:eggs_app/widgets/button.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ActionButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of<AppBloc>(context);
+    
+    switch (bloc.state) {
+      case "stopped" :
+        return Button(
+          label: "START", 
+          selected: true,
+          callback: bloc.start,
+        );
+
+      case "cooking" :
+        return Button(
+          label: "STOP", 
+          selected: true,
+          callback: bloc.stop,
+        );
+
+      case "done" :
+        return Button(
+          label: "ALL DONE", 
+          selected: true,
+          callback: bloc.reset,
+        );
+
+      default :
+        return Button(
+          label: "START", 
+          selected: true,
+          callback: bloc.start,
+        );
+    }
+  }
+}
